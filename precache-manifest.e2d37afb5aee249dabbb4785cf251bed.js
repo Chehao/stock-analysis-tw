23ebbe29300c8145fc2b7718102db2628604e5ed
chehao.hsiao <chehao.hsiao@gmail.com>
2019-08-30 12:52:16 +0800

    Updates

---
+++ precache-manifest.e2d37afb5aee249dabbb4785cf251bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33360b1fbe3d819b530a30e2663c1064",
+    "url": "/stock-analysis-tw/index.html"
+  },
+  {
+    "revision": "db2a2aa49d96231e5d0a",
+    "url": "/stock-analysis-tw/static/css/2.56909a4e.chunk.css"
+  },
+  {
+    "revision": "9890e817f8642221c3d4",
+    "url": "/stock-analysis-tw/static/css/main.128b9cb8.chunk.css"
+  },
+  {
+    "revision": "db2a2aa49d96231e5d0a",
+    "url": "/stock-analysis-tw/static/js/2.85f68a7f.chunk.js"
+  },
+  {
+    "revision": "9890e817f8642221c3d4",
+    "url": "/stock-analysis-tw/static/js/main.422992c0.chunk.js"
+  },
+  {
+    "revision": "422a43d7291fcec3820f",
+    "url": "/stock-analysis-tw/static/js/runtime~main.934530f3.js"
+  }
+]);
