8e055bcc030096e471bb4f78fa750c32e47624d0
chehao.hsiao <chehao.hsiao@gmail.com>
2019-08-30 12:44:23 +0800

    add docs

---
+++ docs/precache-manifest.cb84db682bfca6045775e9f399618006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d7f503224103753eae7e7b01f71750",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db2a2aa49d96231e5d0a",
+    "url": "/static/css/2.56909a4e.chunk.css"
+  },
+  {
+    "revision": "b5ae5bd9ebf9a303a4b5",
+    "url": "/static/css/main.128b9cb8.chunk.css"
+  },
+  {
+    "revision": "db2a2aa49d96231e5d0a",
+    "url": "/static/js/2.85f68a7f.chunk.js"
+  },
+  {
+    "revision": "b5ae5bd9ebf9a303a4b5",
+    "url": "/static/js/main.e7b30165.chunk.js"
+  },
+  {
+    "revision": "a3e6c420d64525e19130",
+    "url": "/static/js/runtime~main.617e650a.js"
+  }
+]);
